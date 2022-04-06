154c90daec2ce0cd25083db799ecf1d0a6fce43d
Christie Miller <cnichole@mac.com>
2022-04-06 17:23:46 -0400

    Completing Vue tutorial

---
+++ Vue practice.js
@@ -0,0 +1,10 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: '100% cotton',
+        image: 'vmSocks-green-onWhite.jpeg',
+        link: 'https://rockemsocks.com/products/working-from-home?variant=31887536521276&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&utm_campaign=gs-2019-09-11&utm_source=google&utm_medium=smart_campaign&gclid=EAIaIQobChMI8cvE6KiA9wIVl-azCh1_JAqBEAQYBSABEgIuY_D_BwE',
+        inventory: 8,
+    }
+})
