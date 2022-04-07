18d1bcd272c62514a450e499beb7b7f9ef65a2a0
Christie Miller <cnichole@mac.com>
2022-04-06 20:44:19 -0400

    Added color details

--- Vue practice.js
+++ Vue practice.js
@@ -2,9 +2,18 @@ let app = new Vue ({
     el: '#app',
     data: {
         product: 'Socks',
-        description: '100% cotton',
+        details: ['100% cotton', 'Gender-neutral'],
         image: 'vmSocks-green-onWhite.jpeg',
-        link: 'https://rockemsocks.com/products/working-from-home?variant=31887536521276&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&utm_campaign=gs-2019-09-11&utm_source=google&utm_medium=smart_campaign&gclid=EAIaIQobChMI8cvE6KiA9wIVl-azCh1_JAqBEAQYBSABEgIuY_D_BwE',
-        inventory: 8,
+        inStock: true,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
